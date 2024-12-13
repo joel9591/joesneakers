7428ce6a3381e54416b491f52b0e344bdc054969
joel9591 <joelstalin76@gmail.com>
2024-12-14 00:11:40 +0530

    second setup- seperation of concerns

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
+const cookieParser = require("cookie-parser");
+const path = require('path');
+const db = require('./config/mongoose-connection');
+const ownerRouter = require('./routes/ownerRouter');
+const productsRouter = require('./routes/productsRouter');
+const userRouter = require('./routes/userRouter');
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -9,9 +13,9 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'public')))
 
 
-app.get('/', (req, res)=>{
-  res.render("index");
-})
+app.use("/owners", ownerRouter);
+app.use("/users", userRouter);
+app.use("/products", productsRouter);
 
 app.listen(3000, ()=>{
   console.log("server is running");
